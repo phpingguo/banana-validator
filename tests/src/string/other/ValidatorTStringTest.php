efc9617e6364b19ad90d9ee468bac7690e735cd1
h-sugawara <nnfxt540@gmail.com>
2014-03-08 20:11:43 +0900

    changed string validator class unittest files

--- tests/src/string/other/ValidatorTStringTest.php
+++ tests/src/string/other/ValidatorTStringTest.php
@@ -1,18 +1,20 @@
 <?php
 namespace Phpingguo\BananaValidator\Tests\String\Latin;
 
+use Phpingguo\BananaValidator\Enums\ValidationError;
 use Phpingguo\BananaValidator\Options;
 use Phpingguo\BananaValidator\String\Other\TextString;
+use Phpingguo\BananaValidator\ValidationErrorException;
 
 class ValidatorTStringTest extends \PHPUnit_Framework_TestCase
 {
     public function testInitOptions()
     {
         return function ($option_list) {
-            $options	= Options::getInstance(true);
+            $options = Options::getInstance(true);
             
             foreach ($option_list as $key => $value) {
-                $options	= is_numeric($key) ? $options->$value() : $options->$key($value);
+                $options = is_numeric($key) ? $options->$value() : $options->$key($value);
             }
             
             return $options;
@@ -23,45 +25,31 @@ class ValidatorTStringTest extends \PHPUnit_Framework_TestCase
     {
         return [
             [ 'a', true, [], null ],
-            [ 'z', true, [], null ],
             [ 'A', true, [], null ],
-            [ 'Z', true, [], null ],
             [ 'abc', true, [], null ],
-            [ 'xyz', true, [], null ],
             [ 'ABC', true, [], null ],
-            [ 'XYZ', true, [], null ],
+            [ 'a b c', false, [], [ ValidationError::FORMAT ] ],
+            [ 'X Y Z', false, [], [ ValidationError::FORMAT ] ],
+            [ 'ひらがな', true, [], null ],
+            [ '漢字', true, [], null ],
+            [ 'ｶﾀｶﾅ', true, [], null ],
+            [ 'カタカナ', true, [], null ],
             [ '!"#$%&\'()=~|-^\\[]{}/?_*:;+`@,.<>', true, [], null ],
             [ '1a', true, [], null ],
-            [ 'a1', true, [], null ],
             [ '1Z', true, [], null ],
-            [ 'Z1', true, [], null ],
-            [ 'ひらがな', true, [], null ],
-            [ 'ひらがな1', true, [], null ],
             [ '1ひらがな', true, [], null ],
-            [ '漢字', true, [], null ],
-            [ '漢字1', true, [], null ],
             [ '1漢字', true, [], null ],
-            [ 'ｶﾀｶﾅ', true, [], null ],
-            [ 'ｶﾀｶﾅ1', true, [], null ],
             [ '1ｶﾀｶﾅ', true, [], null ],
-            [ 'カタカナ', true, [], null ],
-            [ 'カタカナ1', true, [], null ],
             [ '1カタカナ', true, [], null ],
             [ 'abcひらがな', true, [], null ],
             [ 'abc漢字', true, [], null ],
             [ 'abcｶﾀｶﾅ', true, [], null ],
             [ 'abcカタカナ', true, [], null ],
-            [ 'ひらがなabc', true, [], null ],
             [ 'ひらがな漢字', true, [], null ],
             [ 'ひらがなｶﾀｶﾅ', true, [], null ],
             [ 'ひらがなカタカナ', true, [], null ],
-            [ '漢字abc', true, [], null ],
-            [ '漢字ひらがな', true, [], null ],
             [ '漢字ｶﾀｶﾅ', true, [], null ],
             [ '漢字カタカナ', true, [], null ],
-            [ 'カタカナabc', true, [], null ],
-            [ 'カタカナひらがな', true, [], null ],
-            [ 'カタカナ漢字', true, [], null ],
             [ 'カタカナｶﾀｶﾅ', true, [], null ],
             [ 'abc', true, [ 'whitespace' ], null ],
             [ 'a b c', true, [ 'whitespace' ], null ],
@@ -81,20 +69,22 @@ class ValidatorTStringTest extends \PHPUnit_Framework_TestCase
             [ 'カタカナ', true, [ 'whitespace' ], null ],
             [ 'カ タ カ ナ', true, [ 'whitespace' ], null ],
             [ 'カ　タ　カ　ナ', true, [ 'whitespace' ], null ],
-            [ 0, false, [ 'nullable' ], 'ValidationErrorException' ],
-            [ 0.0, false, [ 'nullable' ], 'ValidationErrorException' ],
+            [ 0, false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
+            [ 0.0, false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
             [ '0', true, [ 'nullable' ], null ],
             [ null, false, [ 'nullable' ], null ],
             [ '', false, [ 'nullable' ], null ],
             [ false, false, [ 'nullable' ], null ],
             [ [], false, [ 'nullable' ], null ],
-            [ 0, false, [], 'ValidationErrorException' ],
-            [ 0.0, false, [], 'ValidationErrorException' ],
+            [ 0, false, [], [ ValidationError::FORMAT ] ],
+            [ 0.0, false, [], [ ValidationError::FORMAT ] ],
             [ '0', true, [], null ],
-            [ null, false, [], 'ValidationErrorException' ],
-            [ '', false, [], 'ValidationErrorException' ],
-            [ false, false, [], 'ValidationErrorException' ],
-            [ [], false, [], 'ValidationErrorException' ],
+            [ null, false, [], [ ValidationError::INVALID ] ],
+            [ '', false, [], [ ValidationError::INVALID ] ],
+            [ true, false, [], [ ValidationError::FORMAT ] ],
+            [ false, false, [], [ ValidationError::INVALID ] ],
+            [ [], false, [], [ ValidationError::INVALID ] ],
+            [ new \stdClass(), true, [], [ ValidationError::INVALID ] ],
         ];
     }
     
@@ -104,8 +94,10 @@ class ValidatorTStringTest extends \PHPUnit_Framework_TestCase
      */
     public function testValidate($value, $expected, $options, $exception, $init)
     {
-        isset($exception) && $this->setExpectedException("Phpingguo\\BananaValidator\\" . $exception);
-        
-        $this->assertSame($expected, (new TextString())->validate($value, $init($options)));
+        try {
+            $this->assertSame($expected, (new TextString())->validate($value, $init($options)));
+        } catch (ValidationErrorException $e) {
+            $this->assertSame($exception, $e->getErrorLists());
+        }
     }
 }
