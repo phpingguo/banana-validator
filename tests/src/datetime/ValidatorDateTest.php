7606011e0949aec48531d28c3c55b07a46e1d5f2
h-sugawara <nnfxt540@gmail.com>
2014-03-09 01:05:58 +0900

    changed datetime validator class unittest files

--- tests/src/datetime/ValidatorDateTest.php
+++ tests/src/datetime/ValidatorDateTest.php
@@ -2,17 +2,19 @@
 namespace Phpingguo\BananaValidator\Tests\DateTime;
 
 use Phpingguo\BananaValidator\DateTime\Date;
+use Phpingguo\BananaValidator\Enums\ValidationError;
 use Phpingguo\BananaValidator\Options;
+use Phpingguo\BananaValidator\ValidationErrorException;
 
 class ValidatorDateTest extends \PHPUnit_Framework_TestCase
 {
     public function testInitOptions()
     {
         return function ($option_list) {
-            $options	= Options::getInstance(true);
+            $options = Options::getInstance(true);
             
             foreach ($option_list as $key => $value) {
-                $options	= is_numeric($key) ? $options->$value() : $options->$key($value);
+                $options = is_numeric($key) ? $options->$value() : $options->$key($value);
             }
             
             return $options;
@@ -22,39 +24,59 @@ class ValidatorDateTest extends \PHPUnit_Framework_TestCase
     public function providerValidate()
     {
         return [
-            [ '2000-01-01 00:00:00', false, [], 'ValidationErrorException' ],
-            [ '2000-01-01 00:00', false, [], 'ValidationErrorException' ],
-            [ '2000-01-01 000000', false, [], 'ValidationErrorException' ],
-            [ '2000-01-01 0000', false, [], 'ValidationErrorException' ],
-            [ '2000/01/01 00:00:00', false, [], 'ValidationErrorException' ],
-            [ '2000/01/01 00:00', false, [], 'ValidationErrorException' ],
-            [ '2000/01/01 000000', false, [], 'ValidationErrorException' ],
-            [ '2000/01/01 0000', false, [], 'ValidationErrorException' ],
-            [ '20000101 00:00:00', false, [], 'ValidationErrorException' ],
-            [ '20000101 00:00', false, [], 'ValidationErrorException' ],
-            [ '20000101 000000', false, [], 'ValidationErrorException' ],
-            [ '20000101 0000', false, [], 'ValidationErrorException' ],
+            [ '2000-01-01 00:00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000-01-01 00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000-01-01 000000', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000-01-01 0000', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000/01/01 00:00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000/01/01 00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000/01/01 000000', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000/01/01 0000', false, [], [ ValidationError::FORMAT ] ],
+            [ '20000101 00:00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '20000101 00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '20000101 000000', false, [], [ ValidationError::FORMAT ] ],
+            [ '20000101 0000', false, [], [ ValidationError::FORMAT ] ],
             [ '2000-01-01', true, [], null ],
             [ '2000/01/01', true, [], null ],
             [ '20000101', true, [], null ],
-            [ '00:00:00', false, [], 'ValidationErrorException' ],
-            [ '00:00', false, [], 'ValidationErrorException' ],
-            [ '000000', false, [], 'ValidationErrorException' ],
-            [ '0000', false, [], 'ValidationErrorException' ],
-            [ 0, false, [ 'nullable' ], 'ValidationErrorException' ],
-            [ 0.0, false, [ 'nullable' ], 'ValidationErrorException' ],
-            [ '0', false, [ 'nullable' ], 'ValidationErrorException' ],
+            [ '2000-01-01', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MIN ] ],
+            [ '2000/01/01', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MIN ] ],
+            [ '20000101', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MIN ] ],
+            [ '2005-02-01', true, [ 'min' => '20050101', 'max' => '20061231' ], null ],
+            [ '2005/02/01', true, [ 'min' => '20050101', 'max' => '20061231' ], null ],
+            [ '20050201', true, [ 'min' => '20050101', 'max' => '20061231' ], null ],
+            [ '2010-01-01', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MAX ] ],
+            [ '2010/01/01', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MAX ] ],
+            [ '20100101', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MAX ] ],
+            [ '1999-01-01', false, [], [ ValidationError::FORMAT, ValidationError::MIN ] ],
+            [ '1999/01/01', false, [], [ ValidationError::FORMAT, ValidationError::MIN ] ],
+            [ '19990101', false, [], [ ValidationError::FORMAT, ValidationError::MIN ] ],
+            [ '10000-01-01', false, [], [ ValidationError::FORMAT ] ],
+            [ '10000/01/01', false, [], [ ValidationError::FORMAT ] ],
+            [ '100000101', false, [], [ ValidationError::FORMAT ] ],
+            [ '9999-13-01', false, [], [ ValidationError::FORMAT ] ],
+            [ '9999/13/01', false, [], [ ValidationError::FORMAT ] ],
+            [ '99991301', false, [], [ ValidationError::FORMAT ] ],
+            [ '00:00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '000000', false, [], [ ValidationError::FORMAT ] ],
+            [ '0000', false, [], [ ValidationError::FORMAT ] ],
+            [ 0, false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
+            [ 0.0, false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
+            [ '0', false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
             [ null, false, [ 'nullable' ], null ],
             [ '', false, [ 'nullable' ], null ],
             [ false, false, [ 'nullable' ], null ],
             [ [], false, [ 'nullable' ], null ],
-            [ 0, false, [], 'ValidationErrorException' ],
-            [ 0.0, false, [], 'ValidationErrorException' ],
-            [ '0', false, [], 'ValidationErrorException' ],
-            [ null, false, [], 'ValidationErrorException' ],
-            [ '', false, [], 'ValidationErrorException' ],
-            [ false, false, [], 'ValidationErrorException' ],
-            [ [], false, [], 'ValidationErrorException' ],
+            [ 0, false, [], [ ValidationError::FORMAT ] ],
+            [ 0.0, false, [], [ ValidationError::FORMAT ] ],
+            [ '0', false, [], [ ValidationError::FORMAT ] ],
+            [ null, false, [], [ ValidationError::INVALID ] ],
+            [ '', false, [], [ ValidationError::INVALID ] ],
+            [ true, false, [], [ ValidationError::FORMAT ] ],
+            [ false, false, [], [ ValidationError::INVALID ] ],
+            [ [], false, [], [ ValidationError::INVALID ] ],
+            [ new \stdClass(), false, [], [ ValidationError::INVALID ] ],
         ];
     }
     
@@ -64,8 +86,10 @@ class ValidatorDateTest extends \PHPUnit_Framework_TestCase
      */
     public function testValidate($value, $expected, $options, $exception, $init)
     {
-        isset($exception) && $this->setExpectedException("Phpingguo\\BananaValidator\\" . $exception);
-        
-        $this->assertSame($expected, (new Date())->validate($value, $init($options)));
+        try {
+            $this->assertSame($expected, (new Date())->validate($value, $init($options)));
+        } catch (ValidationErrorException $e) {
+            $this->assertSame($exception, $e->getErrorLists());
+        }
     }
 }
--- tests/src/datetime/ValidatorDateTimeTest.php
+++ tests/src/datetime/ValidatorDateTimeTest.php
@@ -2,17 +2,19 @@
 namespace Phpingguo\BananaValidator\Tests\DateTime;
 
 use Phpingguo\BananaValidator\DateTime\DateTime;
+use Phpingguo\BananaValidator\Enums\ValidationError;
 use Phpingguo\BananaValidator\Options;
+use Phpingguo\BananaValidator\ValidationErrorException;
 
 class ValidatorDateTimeTest extends \PHPUnit_Framework_TestCase
 {
     public function testInitOptions()
     {
         return function ($option_list) {
-            $options	= Options::getInstance(true);
+            $options = Options::getInstance(true);
             
             foreach ($option_list as $key => $value) {
-                $options	= is_numeric($key) ? $options->$value() : $options->$key($value);
+                $options = is_numeric($key) ? $options->$value() : $options->$key($value);
             }
             
             return $options;
@@ -34,27 +36,42 @@ class ValidatorDateTimeTest extends \PHPUnit_Framework_TestCase
             [ '20000101 00:00', true, [], null ],
             [ '20000101 000000', true, [], null ],
             [ '20000101 0000', true, [], null ],
-            [ '2000-01-01', false, [], 'ValidationErrorException' ],
-            [ '2000/01/01', false, [], 'ValidationErrorException' ],
-            [ '20000101', false, [], 'ValidationErrorException' ],
-            [ '00:00:00', false, [], 'ValidationErrorException' ],
-            [ '00:00', false, [], 'ValidationErrorException' ],
-            [ '000000', false, [], 'ValidationErrorException' ],
-            [ '0000', false, [], 'ValidationErrorException' ],
-            [ 0, false, [ 'nullable' ], 'ValidationErrorException' ],
-            [ 0.0, false, [ 'nullable' ], 'ValidationErrorException' ],
-            [ '0', false, [ 'nullable' ], 'ValidationErrorException' ],
+            [ '2000-01-01 00:00:00', false, [ 'min' => '20050101 000000' ], [ ValidationError::MIN ] ],
+            [ '2005-01-01 00:00:01', true, [ 'min' => '20050101 000000' ], null ],
+            [ '2010-01-01 00:00:00', false, [ 'max' => '20061231 235959' ], [ ValidationError::MAX ] ],
+            [ '2005-01-01 00:00:00', true, [ 'max' => '20061231 235959' ], null ],
+            [ '2000-01-01 00:00:00', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MIN ] ],
+            [ '2000/01/01 00:00:00', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MIN ] ],
+            [ '20000101 000000', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MIN ] ],
+            [ '2005-02-01 01:00:00', true, [ 'min' => '20050101', 'max' => '20061231' ], null ],
+            [ '2005/02/01 01:00:00', true, [ 'min' => '20050101', 'max' => '20061231' ], null ],
+            [ '20050201 010000', true, [ 'min' => '20050101', 'max' => '20061231' ], null ],
+            [ '2010-01-01 00:00:00', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MAX ] ],
+            [ '2010/01/01 00:00:00', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MAX ] ],
+            [ '20100101 000000', false, [ 'min' => '20050101', 'max' => '20061231' ], [ ValidationError::MAX ] ],
+            [ '2000-01-01', false, [], [ ValidationError::FORMAT ] ],
+            [ '2000/01/01', false, [], [ ValidationError::FORMAT ] ],
+            [ '20000101', false, [], [ ValidationError::FORMAT ] ],
+            [ '00:00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '00:00', false, [], [ ValidationError::FORMAT ] ],
+            [ '000000', false, [], [ ValidationError::FORMAT ] ],
+            [ '0000', false, [], [ ValidationError::FORMAT ] ],
+            [ 0, false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
+            [ 0.0, false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
+            [ '0', false, [ 'nullable' ], [ ValidationError::FORMAT ] ],
             [ null, false, [ 'nullable' ], null ],
             [ '', false, [ 'nullable' ], null ],
             [ false, false, [ 'nullable' ], null ],
             [ [], false, [ 'nullable' ], null ],
-            [ 0, false, [], 'ValidationErrorException' ],
-            [ 0.0, false, [], 'ValidationErrorException' ],
-            [ '0', false, [], 'ValidationErrorException' ],
-            [ null, false, [], 'ValidationErrorException' ],
-            [ '', false, [], 'ValidationErrorException' ],
-            [ false, false, [], 'ValidationErrorException' ],
-            [ [], false, [], 'ValidationErrorException' ],
+            [ 0, false, [], [ ValidationError::FORMAT ] ],
+            [ 0.0, false, [], [ ValidationError::FORMAT ] ],
+            [ '0', false, [], [ ValidationError::FORMAT ] ],
+            [ null, false, [], [ ValidationError::INVALID ] ],
+            [ '', false, [], [ ValidationError::INVALID ] ],
+            [ true, false, [], [ ValidationError::FORMAT ] ],
+            [ false, false, [], [ ValidationError::INVALID ] ],
+            [ [], false, [], [ ValidationError::INVALID ] ],
+            [ new \stdClass(), false, [], [ ValidationError::INVALID ] ],
         ];
     }
     
@@ -64,8 +81,10 @@ class ValidatorDateTimeTest extends \PHPUnit_Framework_TestCase
      */
     public function testValidate($value, $expected, $options, $exception, $init)
     {
-        isset($exception) && $this->setExpectedException("Phpingguo\\BananaValidator\\" . $exception);
-        
-        $this->assertSame($expected, (new DateTime())->validate($value, $init($options)));
+        try {
+            $this->assertSame($expected, (new DateTime())->validate($value, $init($options)));
+        } catch (ValidationErrorException $e) {
+            $this->assertSame($exception, $e->getErrorLists());
+        }
     }
 }
